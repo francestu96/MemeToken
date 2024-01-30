d6823a4695fc86f38329a76143dda7f30ad99657
francestu96 <francestu96@gmail.com>
2024-01-30 20:45:21 +0100

    CA

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 const AppConfig = {
-    tokenAddress: undefined,
-    telegramURL: "https://t.me/coomin_soon!",
+    tokenAddress: "5xpF2iJgxRvUEtSs6oakczaPZWGvm6bpXoVAU6cKzahq",
+    telegramURL: "https://t.me/ChowChow_Solana_Portal",
     twitterULR: "https://twitter.com/cooming_soon!",
     launchUTC: "Tue 30 20:00"
 }
